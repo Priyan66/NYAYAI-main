edbc8adb22e3db5c2dbf88010166d39b2f405483
Priyan66 <priyanshuvs66@gmail.com>
2026-06-27 17:13:46 +0530

    fix: explicit database url in prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,9 +7,13 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error'] : [],
+    log: ['query', 'error', 'warn'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
 export default prisma;
